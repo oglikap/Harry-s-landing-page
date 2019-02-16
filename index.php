083efb9cba98c7d24eec7314525fcf5604273007
oglikap <kroon.rj@gmail.com>
2019-02-16 13:13:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welkom bij Harry's wereld</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body style="background: #ffff48;">
+    <div class="bs-container">
+      <div class="bs-flexbox bs-height-vh">
+        <h1 class="bs-font-hero">Krijg<br> nou<br> tieten<br><span style="font-size: 2em; color: #840f0a;">!</span></h1>
+      </div>
+    </div>
+  </body>
+</html>
